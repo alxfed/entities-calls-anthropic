2b68365889db8628a7c8ae3a34d41e9be0d9d612
Alex Fedotov <alxfed@gmail.com>
2024-04-11 19:16:27 -0500

    initial commit

--- entities/memorizing_machine.py
+++ entities/memorizing_machine.py
@@ -6,15 +6,23 @@ This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
 from os import getenv
+from util import githubf as ghf
 from yaml import load, FullLoader
 
 
 if getenv("GITHUB_TOKEN") is not None:
-    print('github token is here')
-
-memm = """
-name: memory_machine
-description: Remembers everything you ask it to remember and recalls it later, if you ask it to.
+    organization = 'memorizing-machine'
+    repo = 'memorizing-machine-private'
+    file = 'machine.yaml'
+    repo = ghf.creupdate_repo(organization=organization,
+                              repository_name=repo,
+                              private=True)
+    memm = ghf.read_file(repository=repo,
+                         file_path=file)
+else:
+    memm = """
+name: memorizing_machine
+description: Memorizes everything you ask it to remember and recalls it later, if you ask it to.
 input_schema:
   type: object
   properties:
@@ -28,5 +36,8 @@ input_schema:
       type: string
       description: The description of the thing to be recalled. This parameter should only be used if you need to recall something that you have memorized. Do not use this parameter if you are trying to memorize something.
 """
+MEMORIZING_MACHINE = load(memm, Loader=FullLoader)
 
-MEMORY_MACHINE = load(memm, Loader=FullLoader)
+
+if __name__ == "__main__":
+    print(MEMORIZING_MACHINE)
